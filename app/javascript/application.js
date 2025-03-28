a8ebf9c28d0818ad657a540cf40a7047205365dd
nezccommand <nezu9000@gmail.com>
2025-03-29 02:25:26 +0900

    fix:トップページへボタンを押しても画面遷移が上手くいかない不具合を修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,10 +6,44 @@ import * as bootstrap from "bootstrap"
 //= require swiper.js
 
 document.addEventListener('DOMContentLoaded', function() {
-  const retryButtons = document.querySelectorAll('.retry-button');
-  retryButtons.forEach(button => {
-    button.addEventListener('click', function() {
-      window.location.href = '/';
+  const retryButton = document.getElementById('retry-button');
+  if (retryButton) {
+    retryButton.addEventListener('click', function() {
+      window.location.href = '/'; // トップページに強制リダイレクト
     });
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const quizStartButton = document.querySelector('a[data-reload="true"]');
+  
+  if (quizStartButton) {
+    quizStartButton.addEventListener('click', function(event) {
+      // リダイレクト後にページをリロードする処理
+      event.preventDefault();  // デフォルトの遷移をキャンセル
+      window.location.href = this.href;  // リダイレクト後、リロードする
+      setTimeout(function() {
+        location.reload();  // 1秒後にリロード
+      }, 1000);
+    });
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const retryButton = document.getElementById('result-retry-button');
+  retryButton.addEventListener('click', function() {
+    window.location.href = '/';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Swiper 初期化
+  const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    slidesPerView: 2,
+    slidesPerGroup: 1,
   });
 });
