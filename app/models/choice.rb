11ec539b857d421133d238bbe9b04d056bf6be8c
nezccommand <nezu9000@gmail.com>
2025-03-29 16:08:53 +0900

    modelとseedの修正

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,5 +1,5 @@
 class Choice < ApplicationRecord
-  belongs_to :question
+  belongs_to :question, dependent: :destroy
 
   def image_path
     image.present? ? ActionController::Base.helpers.asset_path(image) : nil
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-Question.delete_all
-Choice.delete_all
-
-questions = Question.create!([
+questions = [
   {
     content: "1問目です",
     explanation: "全て室内の写真なので少し難しいです。正解の写真は、よく見ると毛並みが少し違います。",
@@ -17,9 +14,17 @@ questions = Question.create!([
     explanation: "もう1枚子猫の写真がありましたが、そちらは生まれたての頃のこむちゃんの写真です。",
     correct_image: "choice3_3.jpg"
   }
-])
+]
 
-choices = Choice.create!([
+questions = questions.map do |q|
+  Question.find_or_create_by(content: q[:content]) do |question|
+    question.explanation = q[:explanation]
+    question.correct_image = q[:correct_image]
+  end
+end
+
+# Choiceの作成
+choices = [
   { question: questions[0], image: "choice1_1.jpg", correct: false },
   { question: questions[0], image: "choice1_2.jpg", correct: false },
   { question: questions[0], image: "choice1_3.jpg", correct: false },
@@ -34,6 +39,12 @@ choices = Choice.create!([
   { question: questions[2], image: "choice3_2.jpg", correct: false },
   { question: questions[2], image: "choice3_3.jpg", correct: true },
   { question: questions[2], image: "choice3_4.jpg", correct: false }
-])
+]
+
+choices.each do |choice|
+  Choice.find_or_create_by(question: choice[:question], image: choice[:image]) do |c|
+    c.correct = choice[:correct]
+  end
+end
 
 puts "✅問題が登録されました"
