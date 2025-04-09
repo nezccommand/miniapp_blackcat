239c1de6a775f17549e7fcf244dff3492aba2cd7
nezccommand <nezu9000@gmail.com>
2025-04-09 12:58:51 +0900

    add:新しいseed

--- app/controllers/komkomcat_quizzes_controller.rb
+++ app/controllers/komkomcat_quizzes_controller.rb
@@ -1,56 +1,50 @@
-class BlackcatQuizzesController < ApplicationController
+class KomkomcatQuizzesController < ApplicationController
   before_action :set_questions, only: [ :show ]
 
-  def top
-    session[:question_index] = 0
-    session[:answers] = []
-  end
-
   def show
-    @question = @questions[session[:question_index]]
+    @difficult_question = @difficult_questions[session[:dif_question_index]]
   end
 
   def answer
-    @questions = Question.all
-    @question = @questions.find(params[:id])
+    @difficult_questions = DifficultQuestion.all
+    @difficult_question = @difficult_questions.find(params[:id])
 
-    choice = Choice.find(params[:choice_id])
+    choice = @difficult_question.choices.find(params[:choice_id])
 
-    add_or_update_answer(@question.id, choice.id, choice.correct)
-    next_question_index = @questions.index(@question) + 1
+    add_or_update_answer(@difficult_question.id, choice.id, choice.correct)
+    next_question_index = @difficult_questions.index(@difficult_question) + 1
 
-    if next_question_index >= @questions.count
-      redirect_to result_blackcat_quiz_path
+    if next_question_index >= @difficult_questions.count
+      redirect_to result_komkomcat_quiz_path
     else
-      session[:question_index] = next_question_index
-      redirect_to blackcat_quiz_path(session[:question_index])
+      session[:dif_question_index] = next_question_index
+      redirect_to komkomcat_quiz_path(session[:dif_question_index])
     end
   end
 
   def result
-    @answers = session[:answers]
+    @answers = session[:dif_answers]
   end
 
   private
 
   def set_questions
-    @questions = Question.includes(:choices).order(:id)
+    @difficult_questions = DifficultQuestion.includes(:choices).order(:id)
   end
 
-  def add_or_update_answer(question_id, choice_id, correct)
+  def add_or_update_answer(difficult_question_id, choice_id, correct)
     updated = false
-    session[:answers] = session[:answers].map do |answer|
-      if answer["question_id"] == question_id
+    session[:dif_answers] = session[:dif_answers].map do |answer|
+      if answer["question_id"] == difficult_question_id
         updated = true
-        { question_id: question_id, choice_id: choice_id, correct: correct }
+        { question_id: difficult_question_id, choice_id: choice_id, correct: correct }
       else
         answer
       end
     end
 
     unless updated
-      session[:answers] << { "question_id" => question_id, "choice_id" => choice_id, "correct" => correct }
+      session[:dif_answers] << { "question_id" => difficult_question_id, "choice_id" => choice_id, "correct" => correct }
     end
   end
 end
-# 並び替えゲームを作る
